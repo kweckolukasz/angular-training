16e66312bffe774589d0b8bdbad4c096580363ce
kweckolukasz <kweckolukasz@gmail.com>
2022-02-07 14:36:12 +0100

    responsive media query

--- src/app/presentation/grid/grid.component.ts
+++ src/app/presentation/grid/grid.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+  styleUrls: ['./grid.component.css', './grid-responsive.component.css'],
 })
 export class GridComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
